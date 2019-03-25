037b46c0b5cac5e44e260e260e10cc06b4bff812
17021085 <37399595+17021085@users.noreply.github.com>
2019-03-25 21:19:44 +0700

    Revert "trung"
    This reverts commit 62d7f0e069eb165496a57e48d21e8fb2a933848d.

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-var express = require("express");
-	// test router
-	// var router = express.Router();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-app.use(express.static("public"));
-app.set("view engine","ejs");
-app.set("views","./views");
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-var server = require("http").Server(app);
-server.listen(3001);
-// var things = require('./things.js');
-// app.use('/things',things);
-// app.listen(8080);
 
-app.get("/", function(req, res){
-	let data = [
-		{
-			user: 'tuan'
-		},
-		{
-			user: 'tuan'
-		},
-		{
-			user: 'tuan'
-		},
-		{
-			user: 'tuan'
-		},
-		{
-			user: 'tuan'
-		},
-		{
-			user: 'tuan'
-		}
-	];
-	app.locals.data=data;
-	res.render('trangchu',);
-	//res.render("footer");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-// app.get("/things", function(req, res){
-// 	res.render('login');
-// });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
